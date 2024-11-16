65571f137f2d966ad91671aa541d57c4239ef9b8
Tanaka Mawere <tanaka@tanakamawere.co.zw>
2024-11-16 17:44:23 +0200

    fix navbar bug, edited form

--- assets/js/self-scripts.js
+++ assets/js/self-scripts.js
@@ -1,10 +1,9 @@
 function sendToWhatsApp() {
     // Get the name and phone values
     const name = document.getElementById("nameInput").value;
-    const phone = document.getElementById("phoneInput").value;
 
     // Encode the message to be sent on WhatsApp
-    const message = `Hello, my name is ${encodeURIComponent(name)} and my phone number is ${encodeURIComponent(phone)} I would like....`;
+    const message = `Hello, my name is ${encodeURIComponent(name)}. I would like....`;
 
     // Define the WhatsApp API link with the message
     const whatsappUrl = `https://wa.me/263785468923?text=${message}`;
